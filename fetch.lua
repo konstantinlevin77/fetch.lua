19481a7e45d4f840adea82f828360f2e63da47b3
konstantinlevin77 <60093612+konstantinlevin77@users.noreply.github.com>
2022-02-27 20:17:34 +0300

    The function which returns the name of the operating system has been written.

---
+++ fetch.lua
@@ -0,0 +1,15 @@
+function getOSName()
+    
+    local OSName
+    local f = io.open("/etc/os-release","r")
+
+    for line in f:lines() do
+        if (not (string.find(line,"PRETTY_NAME") == nil)) then
+            -- 13 is the index of the char which is at the start
+            -- of the name of operating system.
+            OSName = string.sub(line,14,string.len(line)-1)
+        end 
+    end    
+    f:close()
+    return OSName
+end
