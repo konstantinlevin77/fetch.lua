705be00cef20cc4eebaa186fe8e810330e847956
konstantinlevin77 <60093612+konstantinlevin77@users.noreply.github.com>
2022-02-27 19:32:06 +0300

    Base config file structure has been created.

---
+++ fetch.config.lua
@@ -0,0 +1,28 @@
+_M = {}
+
+_M.titles = {
+    
+    -- Displays the main title consists of username and hostname.
+    mainTitle = true,
+    -- Displays the name of the operating system.
+    OSTitle = true,
+    -- Displays the kernel version
+    kernelVersion = true,
+    -- Displays the uptime.
+    uptime = true,
+    -- Displays the current shell.
+    shell = true,
+    -- Displays the desktop environment.
+    desktopEnvironment = true,
+    -- Displays the window manager.
+    windowManager = true,
+    -- Displays the cpu.
+    cpu = true,
+    -- Displays the GPU.
+    gpu = true,
+    -- Displays the currently used memory.
+    memory = true
+    }
+
+
+return _M
